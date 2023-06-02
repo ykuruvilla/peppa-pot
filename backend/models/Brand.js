ebfb4cf64ca105b48fb69f66640a5ddbc1671b51
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-06-02 19:48:20 +0100

    build brand model

---
+++ backend/models/Brand.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+const BrandSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is a required field"],
+      unique: true,
+    },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+  },
+  { timestamps: true }
+);
+
+const Brand = mongoose.model("Brand", BrandSchema);
+module.exports = Brand;
