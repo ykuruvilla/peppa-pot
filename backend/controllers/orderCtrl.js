d54d6379e7ca5274bca9999b260b0f935a7c8cff
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-06-06 19:28:39 +0100

    add stripe implemntation for online payments

--- backend/controllers/orderCtrl.js
+++ backend/controllers/orderCtrl.js
@@ -3,12 +3,15 @@ const Order = require("../models/Order");
 const Product = require("../models/Product");
 const { User } = require("../models/User");
 const CustomError = require("../utils/CustomError");
+const formatOrdersForStripe = require("../utils/stripeFormatter");
+const stripeKey = process.env.STRIPE_KEY;
+const stripe = require("stripe")(stripeKey);
 
 const createNewOrder = asyncErrorHandler(async (req, res, next) => {
   const { orderItems, shippingAddress, totalPrice } = req.body;
   const user = await User.findById(req.userAuthId);
 
-  if (orderItems?.length <= 0) {
+  if (!orderItems || orderItems.length === 0) {
     const error = new CustomError(
       "Your order must contain at least 1 item",
       400
@@ -27,13 +30,23 @@ const createNewOrder = asyncErrorHandler(async (req, res, next) => {
   user.hasShippingAddress = true;
   user.shippingAddress = shippingAddress;
   await user.save();
-  console.log(user);
 
-  res.status(200).json({
-    status: "success",
-    message: "Order created successfully",
-    order: newOrder,
+  //convert order items to have stripe structure
+  const stripeFormattedOrders = formatOrdersForStripe(orderItems);
+
+  const session = await stripe.checkout.sessions.create({
+    line_items: stripeFormattedOrders,
+    mode: "payment",
+    success_url: "http://localhost:3000/success",
+    cancel_url: "http://localhost:3000/success",
   });
+  res.send({ url: session.url });
+
+  // res.status(200).json({
+  //   status: "success",
+  //   message: "Order created successfully",
+  //   order: newOrder,
+  // });
 });
 
 module.exports = { createNewOrder };
--- backend/routes/orderRoutes 2.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createNewOrder } = require("../controllers/orderCtrl");
-const isLoggedIn = require("../middlewares/isLoggedIn");
-
-const orderRoutes = express.Router();
-
-orderRoutes.post("/", isLoggedIn, createNewOrder);
-
-module.exports = orderRoutes;
