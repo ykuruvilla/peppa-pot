70ff5b00343dd320a4251f6533d5f0948013e831
ykuruvilla <yasha.kuruvilla@gmail.com>
2023-06-13 13:51:42 +0100

    configure file upload

---
+++ backend/config/fileUpload.js
@@ -0,0 +1,25 @@
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const cloudinaryCloudName = process.env.CLOUDINARY_CLOUD_NAME;
+const cloudinaryAPIKey = process.env.CLOUDINARY_API_KEY;
+const cloudinarySecret = process.env.CLOUDINARY_API_SECRET;
+
+//configure cloudinary
+cloudinary.config({
+  cloud_name: cloudinaryCloudName,
+  api_key: cloudinaryAPIKey,
+  api_secret: cloudinarySecret,
+});
+
+//create storage engine
+const storage = new CloudinaryStorage({
+  cloudinary,
+  allowedFormats: ["jpg", "png", "jpeg"],
+  params: { folder: "Peppa-pot-api" },
+});
+
+//Init multer with storage engine
+const upload = multer({ storage });
+
+modules.export = upload;
